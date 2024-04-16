c82a08b4472a1e2227b230b313bce01547ec1d14
Charles Humphrey <charleshumphreygarcia05@gmail.com>
2024-04-16 09:54:42 +0800

    Stats Complete

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const imageDisplay = document.getElementById("pokemonImage");
 const inputtedName = document.getElementById("inputtedName");
 const fetchBTN = document.getElementById("fetchBTN");
+const displayhealth = document.getElementById("healthtxt");
+const displayattack = document.getElementById("attacktxt");
+const displaydefense = document.getElementById("defensetxt");
+const displaySpecialAttack = document.getElementById("specialattacktxt");
+const displaySpecialDefense = document.getElementById("specialdefensetxt");
+const displaySpeed = document.getElementById("speedtxt");
 
 async function getData(){
     try{
@@ -10,13 +16,22 @@ async function getData(){
         if(!response.ok){
             throw new Error("Could not fetch resource");
         }
-
+        
         const data = await response.json();
         const pokemonSprite = data.sprites.front_default;
         imageDisplay.src = pokemonSprite;
+        
+        //Display STATS
+        displayhealth.innerText = data.stats[0].base_stat;
+        displayattack.innerText = data.stats[1].base_stat;
+        displaydefense.innerText = data.stats[2].base_stat;
+        displaySpecialAttack.innerText = data.stats[3].base_stat;
+        displaySpecialDefense.innerText = data.stats[4].base_stat;
+        displaySpeed.innerText = data.stats[5].base_stat;
+        console.log(data);
     }
     catch(error){
         console.log(error);
     }
-
+    
 }
